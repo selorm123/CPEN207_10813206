bd95026c62d3b229ff0228d2ff6d3e3d75770028
Selorm Kumedzro <82770488+selorm123@users.noreply.github.com>
2021-04-20 10:24:08 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginApp</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <button class="btn"><a href="login.php">Login</a></button>
+    <button class="btn"><a href="register.php">Sign Up</a></button>
+</body>
+</html>
